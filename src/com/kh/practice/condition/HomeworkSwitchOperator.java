26ec835c43d33f86bf0b56c56822dde18cb96799
user1 <user1@DESKTOP-MPUKNTA>
2022-11-25 17:48:24 +0900

    Operator_Switch uploaded
    숙제한개 완료!

--- src/com/kh/practice/condition/HomeworkSwitchOperator.java
+++ src/com/kh/practice/condition/HomeworkSwitchOperator.java
@@ -10,32 +10,29 @@ public class HomeworkSwitchOperator {
     	int num1 = sc.nextInt();
     	System.out.println("숫자를 한 번 더 입력하세요 : ");
     	int num2 = sc.nextInt();
-    	System.out.println("연산자를 입력하세요");
-    	//System.out.println("1: +");
-    	//System.out.println("2: -");
-    	//System.out.println("3: *");
-    	//System.out.println("4: /");
-    	//System.out.println("5: %");
-        char operator = sc.next().charAt(0);
+    	System.out.println("연산자를 입력해주세요 : ");   	
+        char operator = sc.next().charAt(0); 
         int result = 0;
-        char case = "";
-        switch (result) {
-            case "+" :  
-            	System.out.println(result = num1 + num2);
+        switch (operator) {
+            case '+' :  
+            	result = num1 + num2;
+            	break;         
+            case '-' :
+            	result = num1 - num2;
+            	break;
+            case '*' :
+            	result = num1 * num2;
+            	break;
+            case '/' :
+            	result = num1 / num2;
+            	break;
+            case '%' :
+            	result = num1 % num2;
+            	break;
             	
-            case 2 :
-            	System.out.println(result = num1 - num2);
-            	break;
-            case 3 :
-            	System.out.println(result = num1 * num2);
-            	break;
-            case 4 :
-            	System.out.println(result = num1 / num2);
-            	break;
-            case 5 :
-            	System.out.println(result = num1 % num2);
-            	break;
         }
+        System.out.println(num1 + " " + operator +" "+ num2 + " = " + result);
+      
         
     }
 }
